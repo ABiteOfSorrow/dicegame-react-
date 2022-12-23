e647a14b922a7fc07939b6fed933dc974d0e6250
ABiteOfSorrow <laceican@naver.com>
2022-12-23 20:09:02 +0100

    Added sum, record of dice number function

--- src/App.js
+++ src/App.js
@@ -2,34 +2,46 @@ import { useState } from 'react';
 import Button from './Button';
 import Dice from './Dice';
 
+// For generate random number of dice
 function randomNumber(n) {
-    return Math.ceil(Math.random() * n)
+    return Math.ceil(Math.random() * n);
 }
 
 
 function App() {
 
     const [num, setNum] = useState(1);
+    const [sum, setSum] = useState(0);
+    const [gameHistory, setGameHistory] = useState([]);
 
     let rollDice = () => {
-        let diceNumber = randomNumber(6)
-        console.log(diceNumber)
+        let diceNumber = randomNumber(6);
         setNum(diceNumber);
+        setSum(sum + diceNumber);
+        // console.log(diceNumber);
+        setGameHistory([...gameHistory, diceNumber]);
     }
     
     let resetDice = () => {
-        setNum(1)
+        setNum(1);
+        setSum(0);
+        setGameHistory([]);
     }
 
     return (    
         <div>
-            <div>
+            <div>                
                 <Button onClick={rollDice}>Lancer</Button>
                 <Button onClick={resetDice}>réinitialiser</Button>
             </div>
+            <h2>Moi</h2>
             <Dice color="blue" num={num}/>
+            <h2>Somme</h2>
+            <p>{sum}</p>
+            <h2>Record</h2>
+            <p>{gameHistory.join(', ')}</p>
         </div>
-    )
+    );
 }
 
 export default App;
