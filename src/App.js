e72236acfd7e907cbfdab082a9220e9dd4c6e670
ABiteOfSorrow <laceican@naver.com>
2022-12-25 20:11:05 +0100

    cleaning code

--- src/App.js
+++ src/App.js
@@ -9,33 +9,21 @@ function randomNumber(n) {
 
 function App() {
     // 자식 요소의 state를 부모요소로 올려줌 - state lifting
-    const [myNum, setMyNum] = useState(1);
-    const [mySum, setMySum] = useState(0);
     const [myGameHistory, setMyGameHistory] = useState([]);
-    const [otherNum, setOtherNum] = useState(1);
-    const [otherSum, setOtherSum] = useState(0);
     const [otherGameHistory, setOtherGameHistory] = useState([]);
     
 
     let rollDice = () => {
         let myDiceNumber = randomNumber(6);
         let otherDiceNumber = randomNumber(6);
-        setMyNum(myDiceNumber);
-        setMySum(mySum + myDiceNumber);
         // console.log(myDiceNumber);
         setMyGameHistory([...myGameHistory, myDiceNumber]);
-        setOtherNum(otherDiceNumber);
-        setOtherSum(otherSum + otherDiceNumber);
         // console.log(myDiceNumber);
         setOtherGameHistory([...otherGameHistory, otherDiceNumber]);
     }
     
     let resetDice = () => {
-        setMyNum(1);
-        setMySum(0);
         setMyGameHistory([]);
-        setOtherNum(1);
-        setOtherSum(0);
         setOtherGameHistory([]);
     }
 
@@ -46,8 +34,8 @@ function App() {
             <Button onClick={resetDice}>réinitialiser</Button>
         </div>    
         <div>            
-            <Board name="Moi" color="blue" num={myNum} sum={mySum} gameHistory={myGameHistory}/>
-            <Board name="Toi" color="red" num={otherNum} sum={otherSum} gameHistory={otherGameHistory}/>
+            <Board name="Moi" color="blue" gameHistory={myGameHistory}/>
+            <Board name="Toi" color="red" gameHistory={otherGameHistory}/>
         </div>
     </>
     );
